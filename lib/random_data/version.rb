a4596fa8a65210bb9eeacd615e7e988d5546e8e6
subelsky <subelsky@5dbe199c-8045-4628-879c-93ead0618a56>
2007-09-21 00:41:15 +0000

    new gem setup
    git-svn-id: http://random-data.rubyforge.org/svn@1 5dbe199c-8045-4628-879c-93ead0618a56

---
+++ lib/random_data/version.rb
@@ -0,0 +1,9 @@
+module RandomData #:nodoc:
+  module VERSION #:nodoc:
+    MAJOR = 1
+    MINOR = 0
+    TINY  = 0
+
+    STRING = [MAJOR, MINOR, TINY].join('.')
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+require 'test/unit'
+require 'rubygems'
+require 'mocha'
+require File.dirname(__FILE__) + '/../lib/random_data'
