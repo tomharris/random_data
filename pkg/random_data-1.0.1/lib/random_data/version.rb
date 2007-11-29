2521d3c05509829545f1f32f2933d09f4693d603
subelsky <subelsky@5dbe199c-8045-4628-879c-93ead0618a56>
2007-11-29 16:12:42 +0000

    fixed Manifest file
    git-svn-id: http://random-data.rubyforge.org/svn@4 5dbe199c-8045-4628-879c-93ead0618a56

--- pkg/random_data-1.0.1/lib/random_data/version.rb
+++
@@ -1,9 +0,0 @@
-module RandomData #:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 1
-    MINOR = 0
-    TINY  = 1
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
--- pkg/random_data-1.0.1/test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'test/unit'
-require 'rubygems'
-require 'mocha'
-require File.dirname(__FILE__) + '/../lib/random_data'
