dfb9bb5bc3817f92b91b944b3980c636fb30828a
Tom Harris <thomas.e.harris@gmail.com>
2012-11-22 14:41:59 -0500

    Removed the deprecated method '.state' as it can't work under ruby 1.9.

--- lib/random_data/locations.rb
+++ lib/random_data/locations.rb
@@ -78,18 +78,10 @@ module RandomData
                    ["Washington", "WA"], ["Wisconsin", "WI"], ["West Virginia", "WV"], ["Wyoming", "WY"]]                    
 
     # Returns a state 2-character abbreviation
-    # Random.state = "IL"
+    # Random.state_code = "IL"
     
     def state_code
       @@us_states.rand[1]
-    end
-
-    # This method is deprecated, and will not
-    # work in 1.9.x because Random already defines
-    # state as a private method. Use state_code
-    # instead.
-    def state
-      state_code
     end
 
     # Returns a full state name
